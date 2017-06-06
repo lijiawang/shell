493e24c3e1e8155f8f5dd50261b9f51f13555fbc
lijiawang <978686633@qq.com>
2017-06-06 09:34:36 +0000

    List all the active hosts on the network.

--- ping/ping.sh
+++ ping/ping.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
-for ip in 192.168.23.{1..255};
+if [ $# -ne 2 ];
+then
+    echo "$0 ip_initial ip_end"
+fi
+ip_initial=$1
+ip_end=$2
+ip1=`echo $1|cut -f1-3 -d"."`
+init=`echo $1|cut -f4 -d"."`
+end=`echo $2|cut -f4 -d"."`
+while [ $init -le $end ]
 do
-    ping $ip -c 1 &> /dev/null
-    if [ $? -eq 0 ]
-    then
-        echo $ip is alive
-    fi
+    for ip in $ip1.$init;
+    do
+        ping $ip -c 1 &> /dev/null
+        if [ $? -eq 0 ]
+        then
+            echo $ip is alive
+        fi
+        init=$[$init+1]
+    done
 done
